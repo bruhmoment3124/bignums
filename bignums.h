d1b869c1d8c57b8c340b22191535e11dbfbef7cd
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2023-04-02 11:02:58 -0400

    Add files via upload

--- bignums.h
+++ bignums.h
@@ -11,13 +11,13 @@ void addbignums(int *arr1, int *arr2, int size)
 	int i;
 	for(i = 0; i<size; i++) arr1[i] += arr2[i];
 	
-	int k;
-	for(k = size-1; k>=0; k--) 
+	int j;
+	for(j = size-1; j>=0; j--) 
 	{
-		if(arr1[k] >= 10)
+		if(arr1[j] >= 10)
 		{
-			arr1[k-1]++;
-			arr1[k] -= 10;
+			arr1[j-1]++;
+			arr1[j] -= 10;
 		}
 	}
 }
@@ -27,13 +27,13 @@ void subbignums(int *arr1, int *arr2, int size)
 	int i;
 	for(i = 0; i<size; i++) arr1[i] -= arr2[i];
 	
-	int k;
-	for(k = size-1; k>=0; k--) 
+	int j;
+	for(j = size-1; j>=0; j--) 
 	{
-		if(arr1[k] < 0)
+		if(arr1[j] < 0)
 		{
-			arr1[k-1]--;
-			arr1[k] += 10;
+			arr1[j-1]--;
+			arr1[j] += 10;
 		}
 	}
 }
@@ -41,42 +41,36 @@ void subbignums(int *arr1, int *arr2, int size)
 void multbignums(int *arr1, int *arr2, int size)
 {
 	int temp1[size];
-	int a;
-	for(a = 0; a<size; a++) temp1[a] = 0;
-	
 	int i;
-	for(i = size-1; i>=0; i--)
+	for(i = 0; i<size; i++) temp1[i] = 0;
+	
+	int j;
+	for(j = size-1; j>=0; j--)
 	{
 		int temp2[size];
-		int j;
-		for(j = 0; j<size; j++) temp2[j] = 0;
-		
 		int k;
-		for(k = 0; k<arr2[i]; k++) addbignums(temp2, arr1, size);
+		for(k = 0; k<size; k++) temp2[k] = 0;
 		
-		int l, length = 0;
-		for(l = 0; l<size; l++)
+		int l;
+		for(l = 0; l<arr2[j]; l++) addbignums(temp2, arr1, size);
+		
+		int m, length = 0;
+		for(m = 0; m<size; m++)
 		{
-			if(temp2[l] != 0) 
+			if(temp2[m] != 0) 
 			{
-				length = size - l;
+				length = size - m;
 				break;
 			}
 		}
 		
-		int m;
-		for(m = 0; m<length; m++) temp1[i-m] += temp2[size-1-m];
+		int n;
+		for(n = 0; n<length; n++) temp1[j-n] += temp2[size-1-n];
 	
-		if(temp1[i] >= 10)
+		while(temp1[j] >= 10)
 		{
-			temp1[i-1]++;
-			temp1[i] -= 10;
-		}
-		
-		if(temp1[i] >= 10)
-		{
-			temp1[i-1]++;
-			temp1[i] -= 10;
+			temp1[j-1]++;
+			temp1[j] -= 10;
 		}
 	}
 	setbignums(arr1, temp1, size);
